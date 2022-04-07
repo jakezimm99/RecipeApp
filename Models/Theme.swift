cc9450aebcae2984ae85bbb91704c4b172210fbc
jakezimmerman <jakezimm99@gmail.com>
2022-04-07 13:10:17 -0500

    add in a theme file for color themes

---
+++ Models/Theme.swift
@@ -0,0 +1,35 @@
+//
+//  Theme.swift
+//  Recipe App (iOS)
+//
+//  Created by Jake Zimmerman on 4/7/22.
+//
+
+import SwiftUI
+
+enum Theme : String {
+case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    
+    var accentColor: Color {
+            switch self {
+            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+            case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+            }
+        }
+}
