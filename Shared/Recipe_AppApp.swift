bd44dc66130ec62f3105055cc9515a30a1069489
jakezimmerman <jakezimm99@gmail.com>
2022-04-07 12:52:46 -0500

    Initial Commit

---
+++ Shared/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Shared
+//
+//  Created by Jake Zimmerman on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
